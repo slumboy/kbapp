fb9917b583d310f8fd907978ee3b7b8a3f3feaaa
slur-dev <Taa_wappey@windowslive.com>
2020-02-05 14:39:37 +0700

    update_code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule,NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavbarappComponent } from './navbarapp/navbarapp.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    NavbarappComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NgbAlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navbarapp/navbarapp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbarapp',
+  templateUrl: './navbarapp.component.html',
+  styleUrls: ['./navbarapp.component.css']
+})
+export class NavbarappComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
